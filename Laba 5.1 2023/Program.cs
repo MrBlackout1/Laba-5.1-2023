d0f40d8c6a4ef101421423f0ce1f12c44ebf05e5
MrBlackout1 <mrbobfish0@gmail.com>
2023-04-14 15:21:15 +0300

    first commit

---
+++ Laba 5.1 2023/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static Queue<int> queue = new Queue<int>();
+    static object lockObject = new object();
+    static Random random = new Random();
+
+    static void Main(string[] args)
+    {
+        Thread producerThread = new Thread(Producer);
+        Thread consumerThread = new Thread(Consumer);
+        producerThread.Start();
+        consumerThread.Start();
+        producerThread.Join();
+        consumerThread.Join();
+    }
+
+    static void Producer()
+    {
+        while (true)
+        {
+            int value = random.Next(100);
+            lock (lockObject)
+            {
+                queue.Enqueue(value);
+                Console.WriteLine("Producer enqueued: " + value);
+            }
+            Thread.Sleep(1000);
+        }
+    }
+
+    static void Consumer()
+    {
+        while (true)
+        {
+            int value;
+            lock (lockObject)
+            {
+                if (queue.Count > 0)
+                {
+                    value = queue.Dequeue();
+                    Console.WriteLine("Consumer dequeued: " + value);
+                }
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
